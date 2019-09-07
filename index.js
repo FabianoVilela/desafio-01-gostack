78b6ed28e1466e605c79bb0616a818a7459a89af
Fabiano Vilela <fabiano.vilela@gmail.com>
2019-09-07 11:29:06 -0300

    Create routes

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const express = require('express');
 
 const server = express();
+server.use(express.json);
 
-server.get('/', (req, res) => {
-  return res.send({message: 'Main' })
+const projects = [];
+
+server.get('/projects', (req, res) => {
+  return res.send({message: 'Projects' })
+});
+
+server.post('/projects', (req, res) => {
+  const {id, title, tasks} = res.params;
+
+  return res.send({message: 'Add projects' })
+});
+
+server.post('/projects/:id/tasks:', (req, res) => {
+  return res.send({message: 'Add tasks' })
+});
+
+server.put('/:id', (req, res) => {
+  return res.send({message: 'Edit' })
+});
+
+server.delete('/:id', (req, res) => {
+  return res.send({message: 'Delete' })
 });
 
 server.listen(3000);
