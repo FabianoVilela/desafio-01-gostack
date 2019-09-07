d0571e53724dbb8f81873f08a3cf85bce42333b4
Fabiano Vilela <fabiano.vilela@gmail.com>
2019-09-07 12:00:01 -0300

    Add and setting eslint and prettier

--- index.js
+++ index.js
@@ -5,26 +5,20 @@ server.use(express.json);
 
 const projects = [];
 
-server.get('/projects', (req, res) => {
-  return res.send({message: 'Projects' })
-});
+server.get('/projects', (req, res) => res.send({ message: 'Projects' }));
 
 server.post('/projects', (req, res) => {
-  const {id, title, tasks} = res.params;
+  const { id, title, tasks } = res.params;
 
-  return res.send({message: 'Add projects' })
+  return res.send({ message: 'Add projects' });
 });
 
-server.post('/projects/:id/tasks:', (req, res) => {
-  return res.send({message: 'Add tasks' })
-});
+server.post('/projects/:id/tasks:', (req, res) =>
+  res.send({ message: 'Add tasks' })
+);
 
-server.put('/:id', (req, res) => {
-  return res.send({message: 'Edit' })
-});
+server.put('/:id', (req, res) => res.send({ message: 'Edit' }));
 
-server.delete('/:id', (req, res) => {
-  return res.send({message: 'Delete' })
-});
+server.delete('/:id', (req, res) => res.send({ message: 'Delete' }));
 
 server.listen(3000);
